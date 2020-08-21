f3775c926f400db4f577fb1a5093a8217b8e022e
macsoftechnologies <macsoftechnologies@gmail.com>
2020-08-21 14:41:06 +0530

    auto sign ui

---
+++ src/app/adddocument/adddocument.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adddocument',
+  templateUrl: './adddocument.component.html',
+  styleUrls: ['./adddocument.component.css']
+})
+export class AdddocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ManageComponent } from './manage/manage.component';
+import { ActionComponent } from './action/action.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { WaitingComponent } from './waiting/waiting.component';
+import { AdddocumentComponent } from './adddocument/adddocument.component';
+
+ 
+const ROUTES : Routes =[
+  
+  {path : ' ', component: HomeComponent},
+  {path : 'home', component: HomeComponent},
+  {path : 'manage', component: ManageComponent},
+  
+  
+  {path : 'adddocument', component: AdddocumentComponent},
+  // {path : 'waiting', component: WaitingComponent},
+
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ManageComponent,
+    ActionComponent,
+    SidenavComponent,
+    WaitingComponent,
+    AdddocumentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
